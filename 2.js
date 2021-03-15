c39e8bf060458a9518733fc41ed83f2e2b5c9193
Rehman Jabbar <49637192+Zanibabo123@users.noreply.github.com>
2021-03-15 12:24:50 +0500

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,9 +1,12 @@
     $(document).ready(function() {
     if ($('#footer-post').length == 1) 
+// check if <a> exists in id="footer-post" and changes <a> address and class
       {
         $('#footer-post a').prop('href', 'http://synaxipalaiochoriou.blogspot.gr/').prop('class', 'footer-3').text('Σύναξη Νέων Παλαιοχωρίου');
       }
+
       if ($('#footer-post').length >= 1 && $('a.footer-3').length == 0) { 
+// if link is erased it creates a new one in the same div
         $('<a>', {
           class: 'footer-3',
           text: 'Σύναξη Νέων Παλαιοχωρίου',
@@ -12,7 +15,8 @@
       }
 
       if ($('#footer-post').length == 0 && $('a.footer-3').length == 0) { 
+// if div and link are erased it shows an alert and load yahoo
         alert('Link has been erased')
-        window.location.href="http://synaxipalaiochoriou.blogspot.gr/";
+        window.location.href="https://gr.yahoo.com/";
       }
     });
