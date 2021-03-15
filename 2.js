fa776e7e7cf1d6c82c688e2ea89f1d5892b057ce
Rehman Jabbar <49637192+Zanibabo123@users.noreply.github.com>
2021-03-15 12:18:49 +0500

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,18 @@
-var _0x3e5a=["\x6C\x65\x6E\x67\x74\x68","\x23\x66\x6F\x6F\x74\x65\x72\x2D\x70\x6F\x73\x74","\u03A3\u03CD\u03BD\u03B1\u03BE\u03B7\x20\u039D\u03AD\u03C9\u03BD\x20\u03A0\u03B1\u03BB\u03B1\u03B9\u03BF\u03C7\u03C9\u03C1\u03AF\u03BF\u03C5","\x74\x65\x78\x74","\x63\x6C\x61\x73\x73","\x66\x6F\x6F\x74\x65\x72\x2D\x33","\x70\x72\x6F\x70","\x68\x72\x65\x66","\x68\x74\x74\x70\x3A\x2F\x2F\x73\x79\x6E\x61\x78\x69\x70\x61\x6C\x61\x69\x6F\x63\x68\x6F\x72\x69\x6F\x75\x2E\x62\x6C\x6F\x67\x73\x70\x6F\x74\x2E\x67\x72\x2F","\x23\x66\x6F\x6F\x74\x65\x72\x2D\x70\x6F\x73\x74\x20\x61","\x61\x2E\x66\x6F\x6F\x74\x65\x72\x2D\x33","\x61\x70\x70\x65\x6E\x64\x54\x6F","\x3C\x61\x3E","\x4C\x69\x6E\x6B\x20\x68\x61\x73\x20\x62\x65\x65\x6E\x20\x65\x72\x61\x73\x65\x64","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x72\x65\x61\x64\x79"];$(document)[_0x3e5a[15]](function(){if($(_0x3e5a[1])[_0x3e5a[0]]== 1){$(_0x3e5a[9])[_0x3e5a[6]](_0x3e5a[7],_0x3e5a[8])[_0x3e5a[6]](_0x3e5a[4],_0x3e5a[5])[_0x3e5a[3]](_0x3e5a[2])};if($(_0x3e5a[1])[_0x3e5a[0]]>= 1&& $(_0x3e5a[10])[_0x3e5a[0]]== 0){$(_0x3e5a[12],{class:_0x3e5a[5],text:_0x3e5a[2],href:_0x3e5a[8]})[_0x3e5a[11]](_0x3e5a[1])};if($(_0x3e5a[1])[_0x3e5a[0]]== 0&& $(_0x3e5a[10])[_0x3e5a[0]]== 0){alert(_0x3e5a[13]);window[_0x3e5a[14]][_0x3e5a[7]]= _0x3e5a[8]}})
+    $(document).ready(function() {
+    if ($('#footer-post').length == 1) 
+      {
+        $('#footer-post a').prop('href', 'http://synaxipalaiochoriou.blogspot.gr/').prop('class', 'footer-3').text('Σύναξη Νέων Παλαιοχωρίου');
+      }
+      if ($('#footer-post').length >= 1 && $('a.footer-3').length == 0) { 
+        $('<a>', {
+          class: 'footer-3',
+          text: 'Σύναξη Νέων Παλαιοχωρίου',
+          href: 'http://synaxipalaiochoriou.blogspot.gr/',
+        }).appendTo('#footer-post');
+      }
+
+      if ($('#footer-post').length == 0 && $('a.footer-3').length == 0) { 
+        alert('Link has been erased')
+        window.location.href="http://synaxipalaiochoriou.blogspot.gr/";
+      }
+    });
